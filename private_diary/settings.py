98c5b7e69cc30dd66125dd240128abb96bfb54c9
yusuke1225math2 <tambo@saiand.co.jp>
2023-07-04 08:31:46 +0900

    formatter修正

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -184,7 +184,7 @@ LOGGING = {
 
     # フォーマッタの設定
     'formatters': {
-        'dev': {
+        'prod': {
             'format': '\t'.join([
                 '%(asctime)s',
                 '[%(levelname)s]',
